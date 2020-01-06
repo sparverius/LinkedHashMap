4d33aa0b388e648169c0a8b81922af4bfaecdde7
Richard K <22059483+sparverius@users.noreply.github.com>
2020-01-06 04:26:27 -0500

    Update LinkedHashMap.ts

--- LinkedHashMap.ts
+++ LinkedHashMap.ts
@@ -1,249 +1,264 @@
-class Entry<V> {
+namespace LHMap {
 
-    public key: string;
-    public value: V;
-    public next?: Entry<V>;
-    public before?: Entry<V>;
-    public after?: Entry<V>;
+    export class LinkedHashMap<V> implements Map<string, V> {
 
-    constructor(key: string, value: V, next = undefined) {
-        this.key = key;
-        this.value = value;
-        this.next = next;
+        private threshold: number;
+        private loadFactor: number;
+        private modCount: number = 0;
 
-        this.before = undefined;
-        this.after = undefined;
-    }
+        public size: number = 0;
+        public buckets: Array<Entry<V>>;
+        public header: Entry<V>;
 
-    remove() {
-        this.before.after = this.after;
-        this.after.before = this.before;
-    }
+        constructor(initialCapacity = 11, loadFactor = 0.75) {
+            this.threshold = Math.floor(initialCapacity * loadFactor);
+            this.loadFactor = loadFactor;
 
-    addBefore(existingEntry: Entry<V>) {
-        this.after = existingEntry;
-        this.before = existingEntry.before;
-        this.before.after = this;
-        this.after.before = this;
-    }
+            this.buckets = new Array(initialCapacity);
 
-    cleanUp() {
-        return this.value;
-    }
-}
-
-class LinkedHashMap<T> {
-
-    private threshold: number;
-    private loadFactor: number;
-    private modCount: number = 0;
-
-    public size: number = 0;
-    public buckets: Array<Entry<T>>;
-    public header: Entry<T>;
-
-    constructor(initialCapacity = 11, loadFactor = 0.75) {
-        this.threshold = Math.floor(initialCapacity * loadFactor);
-        this.loadFactor = loadFactor;
-
-        this.buckets = new Array(initialCapacity);
-
-        this.header = new Entry(undefined, undefined, undefined);
-        this.header.before = this.header;
-        this.header.after = this.header
-    }
-
-    isEmpty() { return this.size === 0; }
-
-    hashCode(str: string) {
-        let hash = 0;
-        for (let i = 0; i < str.length; i++) {
-            hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
-            hash = hash & hash;
-        }
-        return hash;
-    }
-
-    hash(key: string) {
-        return Math.abs(this.hashCode(key) % this.buckets.length);
-    }
-
-    get(key: string): T {
-        let idx = this.hash(key);
-        let entry = this.buckets[idx];
-        while (entry !== undefined) {
-            if (key === entry.key) return entry.value;
-            entry = entry.next;
-        }
-        return undefined;
-    }
-
-    containsKey(key: string): boolean {
-        let idx = this.hash(key);
-        let entry = this.buckets[idx];
-        while (entry !== undefined) {
-            if (key === entry.key)
-                return true;
-            entry = entry.next;
-        }
-        return false;
-    }
-
-    rehash(): void {
-        let newCapacity = (this.buckets.length * 2) + 1;
-        this.threshold = Math.floor(newCapacity * this.loadFactor);
-        let newBuckets = new Array(newCapacity);
-
-        for (let e = this.header.after; e != this.header; e = e.after) {
-            let idx = this.hash(e.key);
-            e.next = newBuckets[idx];
-            newBuckets[idx] = e;
+            this.header = new Entry(undefined, undefined, undefined);
+            this.header.before = this.header;
+            this.header.after = this.header
         }
 
-        this.buckets = newBuckets;
-    }
+        isEmpty(): boolean { return this.size === 0; }
 
-    addEntry(key: string, value: T, idx: number, callRemove: boolean) {
-        let old = this.buckets[idx];
-        let entry = new Entry(key, value, old);
-        this.buckets[idx] = entry;
-        entry.addBefore(this.header);
-    }
+        hashCode(str: string): number {
+            let hash = 0;
+            for (let i = 0; i < str.length; i++) {
+                hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
+                hash = hash & hash;
+            }
+            return hash;
+        }
 
-    put(key: string, value: T): T {
-        if (key === undefined || key === null)
-            throw new Error("put: Key must be valid string")
+        hash(key: string): number {
+            return Math.abs(this.hashCode(key) % this.buckets.length);
+        }
 
-        let idx = this.hash(key);
-        let entry = this.buckets[idx];
-
-        while (entry !== undefined) {
-            if (key === entry.key) {
-                let res = entry.value;
-                entry.value = value;
-                entry.remove();
-                entry.addBefore(this.header);
-                return res;
-            } else {
+        get(key: string): V {
+            let idx = this.hash(key);
+            let entry = this.buckets[idx];
+            while (entry !== undefined) {
+                if (key === entry.key) return entry.value;
                 entry = entry.next;
             }
+            return undefined;
         }
 
-        this.modCount += 1;
-        this.size += 1;
-
-        if (this.size > this.threshold) {
-            this.rehash();
-            idx = this.hash(key);
+        has(key: string): boolean {
+            let idx = this.hash(key);
+            let entry = this.buckets[idx];
+            while (entry !== undefined) {
+                if (key === entry.key)
+                    return true;
+                entry = entry.next;
+            }
+            return false;
         }
 
-        this.addEntry(key, value, idx, true);
+        rehash(): void {
+            let newCapacity = (this.buckets.length * 2) + 1;
+            this.threshold = Math.floor(newCapacity * this.loadFactor);
+            let newBuckets = new Array(newCapacity);
 
-        return undefined;
-    }
+            for (let e = this.header.after; e != this.header; e = e.after) {
+                let idx = this.hash(e.key);
+                e.next = newBuckets[idx];
+                newBuckets[idx] = e;
+            }
 
-    putAll(inputMap: Array<[string, T]>) {
-        if (inputMap === undefined) { return; }
-        for (const entry of inputMap) {
-            let [key, value] = entry;
-            this.put(key, value);
+            this.buckets = newBuckets;
         }
-    }
 
+        addEntry(key: string, value: V, idx: number, callRemove: boolean): void {
+            let old = this.buckets[idx];
+            let entry = new Entry(key, value, old);
+            this.buckets[idx] = entry;
+            entry.addBefore(this.header);
+        }
 
-    remove(key: string): T {
-        let idx = this.hash(key);
-        let entry: Entry<T> = this.buckets[idx];
-        let last = undefined;
+        set(key: string, value: V) {
+            if (key === undefined || key === null)
+                throw new Error("put: Key must be valid string")
 
-        while (entry !== undefined) {
-            if (key === entry.key) {
-                this.modCount += 1;
+            let idx = this.hash(key);
+            let entry = this.buckets[idx];
 
-                if (last === undefined) {
-                    this.buckets[idx] = entry.next;
+            while (entry !== undefined) {
+                if (key === entry.key) {
+                    let res = entry.value;
+                    entry.value = value;
+                    entry.remove();
+                    entry.addBefore(this.header);
+                    return this;
                 } else {
-                    last.next = entry.next;
+                    entry = entry.next;
                 }
-                this.size -= 1;
-
-                return entry.cleanUp();
             }
-            last = entry;
-            entry = entry.next;
-        }
-        return undefined;
-    }
 
-    clear() {
-        if (this.size !== 0) {
             this.modCount += 1;
-            this.buckets = Array(this.size);
-            this.size = 0;
-        }
-    }
+            this.size += 1;
 
-    [Symbol.iterator]() {
-        let knownMod = this.modCount;
-        let count = this.size;
-        let idx = this.buckets.length;
-
-        let nextEntry = this.header.after;
-        let lastReturned = undefined;
-
-
-        let iter_hasNext = () => {
-            return nextEntry !== this.header;
-        }
-
-        let iter_next = () => {
-            let entry = lastReturned = nextEntry;
-            nextEntry = entry.after;
-            return entry;
-        }
-
-        let iter_remove = () => {
-            this.remove(lastReturned.key);
-            lastReturned = undefined;
-        }
-        return {
-            next: () => {
-                if (iter_hasNext()) return { value: iter_next(), done: false };
-                else return { done: true };
+            if (this.size > this.threshold) {
+                this.rehash();
+                idx = this.hash(key);
             }
-        };
-    }
 
-    containsValue(value: T): boolean {
-        for (const entry of this) {
-            if (entry.value === value) {
-                return true;
+            this.addEntry(key, value, idx, true);
+
+            return this;
+        }
+
+        putAll(inputMap: Array<[string, V]>): void {
+            if (inputMap === undefined) { return; }
+            for (const entry of inputMap) {
+                let [key, value] = entry;
+                this.set(key, value);
             }
         }
-        return false;
-    }
 
-    forEach(callback) {
-        for (const entry of this)
-            callback(entry);
-    }
+        delete(key: string): boolean {
+            let idx = this.hash(key);
+            let entry: Entry<V> = this.buckets[idx];
+            let last = undefined;
 
-    map(callback) {
-        const resultArray = [];
+            while (entry !== undefined) {
+                if (key === entry.key) {
+                    this.modCount += 1;
 
-        for (const entry of this) {
-            let res = callback(entry.key, entry.value);
-            resultArray.push(res);
+                    if (last === undefined) {
+                        this.buckets[idx] = entry.next;
+                    } else {
+                        last.next = entry.next;
+                    }
+                    this.size -= 1;
+                    return true;
+                    // return entry.cleanUp();
+                }
+                last = entry;
+                entry = entry.next;
+            }
+            return false;
         }
 
-        return resultArray;
+        clear() {
+            if (this.size !== 0) {
+                this.modCount += 1;
+                this.buckets = Array(this.size);
+                this.size = 0;
+            }
+        }
+
+        forEach(callbackfn: (value: V, key: string, map: Map<string, V>) => void, thisArg?: any): void {
+            let iter = this._iterator();
+            var nxt = iter.next();
+            while (!nxt.done) {
+                callbackfn(nxt.value[1], nxt.value[0], this);
+                nxt = iter.next();
+            }
+        }
+
+        get [Symbol.toStringTag](): string {
+            return "LinkedHashMap";
+        }
+
+        * values(): IterableIterator<V> {
+            let tmp = this.header;
+            let nextEntry = tmp.after;
+
+            while (tmp !== nextEntry) {
+                yield nextEntry.value;
+                nextEntry = nextEntry.after;
+            }
+        }
+
+        * keys(): IterableIterator<string> {
+            let tmp = this.header;
+            let nextEntry = tmp.after;
+
+            while (tmp !== nextEntry) {
+                yield nextEntry.key;
+                nextEntry = nextEntry.after;
+            }
+        }
+
+        private *_iterator(): IterableIterator<[string, V]> {
+            let tmp = this.header;
+            let nextEntry = tmp.after;
+
+            while (tmp !== nextEntry) {
+                yield [nextEntry.key, nextEntry.value];
+                nextEntry = nextEntry.after;
+            }
+        }
+
+        [Symbol.iterator](): IterableIterator<[string, V]> {
+            return this._iterator();
+        }
+
+        entries(): IterableIterator<[string, V]> {
+            return this._iterator();
+        }
+
+        containsValue(value: V): boolean {
+            for (const entry of this) {
+                if (entry[1] === value) return true;
+            }
+            return false;
+        }
+
+        map<R>(callbackfn: (value: V, key: string, map: Map<string, V>) => R): Array<R> {
+            const resultArray: R[] = [];
+
+            for (const entry of this) {
+                let res = callbackfn(entry[1], entry[0], this);
+                resultArray.push(res);
+            }
+
+            return resultArray;
+        }
+
+        foldl(ini, callback) {
+            let result = ini;
+            for (const entry of this) {
+                result = callback(result, [entry[1], entry[0]]);
+            }
+            return result;
+        }
     }
 
-    foldl(ini, callback) {
-        let result = ini;
-        for (const entry of this) {
-            result = callback(result, [entry.key, entry.value]);
+    class Entry<V> {
+
+        public key: string;
+        public value: V;
+        public next?: Entry<V>;
+        public before?: Entry<V>;
+        public after?: Entry<V>;
+
+        constructor(key: string, value: V, next = undefined) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+
+            this.before = undefined;
+            this.after = undefined;
         }
-        return result;
+
+        remove() {
+            this.before.after = this.after;
+            this.after.before = this.before;
+        }
+
+        addBefore(existingEntry: Entry<V>) {
+            this.after = existingEntry;
+            this.before = existingEntry.before;
+            this.before.after = this;
+            this.after.before = this;
+        }
+
+        cleanUp() {
+            return this.value;
+        }
     }
 }
